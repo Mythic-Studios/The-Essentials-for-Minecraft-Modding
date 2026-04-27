7bd30bcdb5906f9c8f12e52ef3944b9ea82a46fe
MarsMyth <callumshelton2503@gmail.com>
2026-04-28 05:17:09 +1000

    Pushing Fabric 26.1.2

---
+++ src/main/java/org/mythic_goose/modding_essentials/TheEssentialsForModdingDataGenerator.java
@@ -0,0 +1,11 @@
+package org.mythic_goose.modding_essentials;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TheEssentialsForModdingDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
